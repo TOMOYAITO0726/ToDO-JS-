23015f7089b4ac350769096b5f7d80d1cabf1f15
tomooyaito <107048356+TOMOYAITO0726@users.noreply.github.com>
2023-10-28 02:06:40 +0000

    css追加、入力フォームの値の取得

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,12 @@
 import "./styles.css";
 
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value; //add-textに入力された値をアラートに表示させる
+  document.getElementById("add-text").value = ""; //テキスト入力・追加後のテキストを空にする
+  alert(inputText);
+  //div生成
+  
+}
+document
+.getElementById("add-button") //対象の要素
+.addEventListener("click", () => onClickAdd());　//種類、関数
