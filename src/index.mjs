5d973fc917fbdc7139c305090234a3ea5f8e910c
tomooyaito <107048356+TOMOYAITO0726@users.noreply.github.com>
2023-10-28 11:26:30 +0000

    テキスト入力値の追加機能

--- src/index.mjs
+++ src/index.mjs
@@ -3,9 +3,18 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value; //add-textに入力された値をアラートに表示させる
   document.getElementById("add-text").value = ""; //テキスト入力・追加後のテキストを空にする
-  alert(inputText);
+  //alert(inputText);
   //div生成
-  
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;//テキストボックスに入力した値が生成される
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
 }
 document
 .getElementById("add-button") //対象の要素
